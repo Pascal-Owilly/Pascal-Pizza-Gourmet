6921b5e2b9ecc93fa3f053aeda5a00dd8a9345bc
Owilly <pasaclouma54@gmail.com>
2022-01-15 12:46:56 +0300

    add checkout button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
-    
-    
+$(document).ready(function(){   
+
+    function Size(small, medium, large){
+       this.small = small;
+       this.meium = medium;
+       this.large = large;
+    }
 });
