19395a546212109c74893c2a54ee2510e78e8797
Owilly <pasaclouma54@gmail.com>
2022-01-15 20:09:37 +0300

    add business logic to target the name and loation

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){   
 
+    // Business logic
+    function UserName(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    function Location(city, county, state) {
+        this.city = city;
+        this.county = county;
+        this.state = state;
+    }
+
+    
+
+
     $("#checkout-btn").click(
         function(){
             $("#section").fadeToggle(1000);
