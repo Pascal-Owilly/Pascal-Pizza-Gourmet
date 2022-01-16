97b791efe655f30d73bd001a678bc9c6217351b3
Owilly <pasaclouma54@gmail.com>
2022-01-16 20:40:42 +0300

    add click listener to the checkout button

--- script.js
+++ script.js
@@ -1,56 +1,202 @@
-$(document).ready(function(){   
+//business logic
 
-    // Business logic
-    function UserName(firstName, lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.locations = [];
-    }
+function checkOut() {
 
-    function Location(town, estate, street) {
-        this.town = town;
-        this.estate = estate;
-        this.street = street;
-    }
-
-    UserName.prototype.fullName = function(){
-        return this.firstName + " " + this.lastName;
-    }
-
-    Location.prototype.fullLocation = function() {
-        return this.town + " " + this.estate + " " + this.street;
-    }
-
-    // User logic
-   $("form#user-input").submit(function(e){
-       e.preventDefault();
-
-       var userFirstName = $("#first-name").val();
-       var userLastName =  $("#last-name").val();
-
-       var newName = new UserName(userFirstName, userLastName);
-   })
-
-     $(".jumbotron").each(function(){
-         var inputedTown = $(this).find("input#town").val();
-         var inputedEstate = $(this).find("input#estate").val();
-         var inputedStreet = $(this).find("input#street").val();
-
-         var newLocation = new Location(inputedTown, inputedEstate, inputedStreet)
-         newName.locations.push(newLocation);
-     });
-
-     
+  var size = (document.getElementById("size").value); 
+  var toppings = (document.getElementById("tops").value);
+  var number = (document.getElementById("quantity").value);
+  var name = document.getElementById("name").value;
+  var crust = (document.getElementById("crusty").value);
 
 
 
-    $("#checkout-btn").click(
-        function(){
-            $("#section").fadeToggle(1000);
-            $("form").submit("")
+  if (size === "Small") {
+    var prize = 650;
+  } else if (size === "Medium") {
+    var prize = 850;
+  } else if (size === "Large") {
+    var prize = 1450;
+  }
+  
 
-        }
-    );
+
+  if (toppings === "Hawaian") {
+    var toppingPrice = 150;
+  } else if (toppings === "Chicken Periperi") {
+    var toppingPrice = 220;
+  } else if (toppings === "BBQ steak") {
+    var toppingPrice = 250;
+  } else if (toppings === "Broccoli") {
+    var toppingPrice = 200;
+  } else if (toppings === "Chicken tikka") {
+    var toppingPrice = 330;
+  }
+
+
+
+  var total = ((prize + toppingPrice) * number);
+
+
+  document.getElementById("name").innerHTML = "Hi" + " " + name + " " + "your order is";
+  document.getElementById("order").innerHTML = number + " " + size + " " + toppings;
+  document.getElementById("total").innerHTML = "costing a total of" + " " + total + "/=";
+
+
+}
+
+
+$(document).ready(function () {
+
+  $('#home-delivery-btn').click(function (e) {
+    e.preventDefault()
+    prompt("enter your location");
+    alert(name + " your order is on the way, delivery fee is ksh 55, thank you")
+  });
+
+  $('#pick-up-btn').click(function () {
+    alert("Hello, we will notify you when your order is ready, thank you" );
+  });
+
+
+  $("#checkout-btn").click(function () {
+    $('#orderPizza').hide();
+    $('#check').show();
+    $("#section").toggle();
+    $("#checkout-btn").toggle();
+    $("#home-delivery-btn").toggle();
+    $("#pick-up-btn").toggle();
+
+  });
+
+ 
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
